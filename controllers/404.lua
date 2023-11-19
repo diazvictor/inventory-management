d67b3e73049f20ae670c280ac45477633e4ccb1c
Díaz Urbaneja Víctor Eduardo Diex <victor.vector008@gmail.com>
2023-11-19 12:09:37 -0400

    Added 404 module (page not found)

--- controllers/404.lua
+++ controllers/404.lua
@@ -8,26 +8,24 @@ Copyright (c) 2020  Díaz  Víctor  aka  (Máster Vitronic)
 <vitronic2@gmail.com>   <mastervitronic@vitronic.com.ve>
 ]]--
 
-local page_not_found	=	class('page_not_found');
-local theme 			    =   conf.theme.theme
+local page_not_found = class('page_not_found');
+local theme = conf.theme.theme
 
 function page_not_found:show_main()
-	view:add_content('title',"Luachi | Dashboard")
-	view:add_content('description',"Luachi, un framework web para Lua")
+	view:add_content('title','Inventory | 404 - Page Not Found')
+	view:add_content('description','Luachi, un framework web para Lua')
 	view:add_contents({
 		js = {
-      ("/js/themes/%s/common/common.min.js"):format(theme),
-			("/js/themes/%s/private/404/404.min.js"):format(theme)
+			('/js/themes/%s/app.min.js'):format(theme),
 		},
 		css  = {
-			("/css/themes/%s/%s.min.css"):format(theme,theme),
-			("/css/themes/%s/common/common.css"):format(theme),
-			("/css/themes/%s/private/404/404.css"):format(theme)
+			('/css/themes/%s/app.min.css'):format(theme,theme),
+			('/css/themes/%s/common/common.css'):format(theme),
+			('/css/themes/%s/public/404.css'):format(theme)
 		}
 	})
-	local page   = template.new(
-		"/private/common/404.html",
-		"/private/page.html"
+	local page = template.new(
+		'/public/404.html'
 	)
 	view:generate(page)
 end
