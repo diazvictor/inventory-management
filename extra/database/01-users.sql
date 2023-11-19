1b045cd436fb93330721f15e3b0220af0836e3c4
Díaz Urbaneja Víctor Eduardo Diex <victor.vector008@gmail.com>
2023-11-19 14:24:07 -0400

    Updated

--- extra/database/01-users.sql
+++ extra/database/01-users.sql
@@ -1,6 +1,6 @@
 /**!
- * @package   Test MySQL
- * @filename  01-people.sql
+ * @package   Inventory Management
+ * @filename  01-users.sql
  * @version   1.0
  * @autor     Díaz Urbaneja Víctor Eduardo Diex <diazvictor@tutamail.com>
  * @date      14.11.2023 10:39:12 -04
@@ -9,12 +9,16 @@
 drop table if exists users;
 create table users (
     id_user 	int not null auto_increment,
-    name 		varchar(50),
-    email		varchar(50),
+    fullname	varchar(16) not null,
+    username	varchar(16) not null,
+    password	char(128) not null,
+    avatar		varchar(200),
+    admin	  	boolean not null default false,
+    status		boolean not null default true,
     primary key (id_user)
 );
 
-insert into users (name, email) values
-('Jose das Couves', 'jose@couves.com'),
-('Manoel Joaquim', 'manoel.joaquim@cafundo.com'),
-('Maria das Dores', 'maria@dores.com')
+insert into users
+	(fullname, username, password, admin)
+values
+	('Víctor Díaz', 'diazvictor', '$5$R21bDnsnhz2$hyQolrqmcnwLuw5quQuLrFiUui3.fruGGyRU1VKYkI5', true)
