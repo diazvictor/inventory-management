f6457fa23ecc06ed0d0b0fd54d560132e4317af8
Díaz Urbaneja Víctor Eduardo Diex <victor.vector008@gmail.com>
2023-12-07 02:13:13 -0400

    Dashboard js

---
+++ public/js/themes/bulma/private/dash/main.src.js
@@ -0,0 +1,11 @@
+/**!
+ * @package   Inventory Management
+ * @filename  main.src.js
+ * @version   1.0
+ * @author    Díaz Urbaneja Víctor Eduardo Diex <diazvictor@tutamail.com>
+ * @date      20.11.2023 00:46:58 -04
+ */
+
+import * as app from '../../common/common.js';
+
+app.start();
